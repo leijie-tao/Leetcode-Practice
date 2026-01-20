f61b12b031365589d8f6fe6e62731bafc535d1f6
Lyna Tao <taoleijie623@gmail.com>
2026-01-20 11:47:17 -0800

    Time: 52 ms (37.44%) | Memory: 22.8 MB (8.62%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,7 +6,10 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        fast = slow = head
+        if not head or not head.next:   #空链表或只有一个节点，则return false
+            return False
+
+        fast = slow = head      #快慢指针同时从头出发，快指针每次2步，慢指针每次1步，相遇则说明循环
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
