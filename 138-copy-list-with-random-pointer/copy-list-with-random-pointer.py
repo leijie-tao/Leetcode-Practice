985f6b63f4cef11d532b822738788838e3c79d9f
Lyna Tao <taoleijie623@gmail.com>
2026-01-22 23:21:00 -0800

    Time: 55 ms (6.24%) | Memory: 20 MB (27.82%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -10,13 +10,15 @@ class Node:
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head: return None
-        #创建哈希表存储克隆对象，
+        #创建哈希表存储克隆对象,每个指针位置对应一个节点
         mapping = {}
         curr = head
         while curr:
             mapping[curr] = Node(curr.val)
             curr = curr.next
         
+        #curr从头开始，mapping[curr].next克隆节点的下一个是mapping中对应curr.next的值
+        #同理，复制random连接
         curr = head
         while curr:
             mapping[curr].next = mapping.get(curr.next)
