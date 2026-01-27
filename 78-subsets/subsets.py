f6db7231c47944b78f53ff565c36b05eee17cdcb
Lyna Tao <taoleijie623@gmail.com>
2026-01-27 10:44:11 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (11.41%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,14 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        path = []
-        def backtrack(start):
-            res.append(path[:])
+        def backtrack(start, path):
+            res.append(path)
             if start == len(nums): #终止条件
                 return
             for i in range(start, len(nums)):
-                path.append(nums[i]) #path内暂存第i位元素
-                backtrack(i + 1) #递归层层向下直到start==len(nums)折返，寻找以i为起点的所有子集
-                path.pop()#每次弹出最后一位数，回到上一层
-        backtrack(0)
+                backtrack(i + 1, path + [nums[i]]) #对每个第i位的数，向下递归层层寻找i+1之后数的子集
+                # path + nums[i]即为初始path+每一层的nums[i]，没有修改path本身因此回溯时不需要pop
+                
+        backtrack(0, [])
         return res
