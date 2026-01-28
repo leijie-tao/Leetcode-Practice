cd404b2c1979fd8a9044885d27a165ab5d19a428
Lyna Tao <taoleijie623@gmail.com>
2026-01-27 16:59:22 -0800

    Time: 3 ms (96.57%) | Memory: 19.7 MB (14.93%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,6 +2,7 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         path = []
+        candidates.sort() 
         def backtrack(remain, start):
             #终止条件
             if remain == 0:
@@ -11,6 +12,8 @@ class Solution:
                 return
             #每层逻辑：遍历每个元素递归寻找符合条件的值
             for i in range(start, len(candidates)):
+                if remain - candidates[i] < 0:
+                    break #优化：先排序，再剪枝（当前位置元素大于remain，则其后都不可能满足条件，直接退出当前循环
                 path.append(candidates[i]) #第i位元素入path，递归从i为起点的范围寻找目标值
                 backtrack(remain - candidates[i], i) #i为起点（可重复利用同一个元素），继续寻找remain
                 path.pop()
