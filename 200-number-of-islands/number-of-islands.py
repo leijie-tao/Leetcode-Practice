8d33b74fc9084e145af34039c03fffbffeec4fe9
LeijieTao <taoleijie623@gmail.com>
2026-02-04 17:02:22 -0800

    Time: 225 ms (94.97%) | Memory: 21.8 MB (42.22%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,3 +1,4 @@
+#BFS解法
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
@@ -5,18 +6,19 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         count = 0
 
-        def dfs(r, c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == "0":
-                return  #终止条件，行/列越界，或节点的值为0
-            grid[r][c] = "0" #淹没当前位置，并dfs向四周扩散
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
-
         for r in range(rows): #遍历表中的每个位置
             for c in range(cols):
-                if grid[r][c] == "1": #遇到陆地则计数，并以该位置为中心向四周淹没，直到边界或水域为止
+                if grid[r][c] == "1": #遇到陆地则计数，把该起点入队，并淹没该起点（必须入队前淹没，否则可能被邻节点视为陆地重新入队）
                     count += 1
-                    dfs(r, c)
+                    queue = deque([(r, c)])
+                    grid[r][c] = "0"
+                    while queue:
+                        curr_r, curr_c = queue.popleft()
+                        #检查curr_r, curr_c的四个方向，如果是边界内且是陆地，则把该节点入队并淹没
+                        for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                            next_r, next_c = curr_r + dr, curr_c + dc
+                            if (0 <= next_r < rows and 0 <= next_c < cols and grid[next_r][next_c] == '1'):
+                                queue.append((next_r, next_c))
+                                grid[next_r][next_c] = "0"
+
         return count
