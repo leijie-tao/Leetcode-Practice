8215f9e247c25383bd6cd4bd222a95db71447ffe
LeijieTao <taoleijie623@gmail.com>
2026-03-10 17:00:36 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (85.27%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,10 +1,17 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        #brute force
+        #binary search:在[0，x]区间找整数k，k^2 <= x and (k+1)^2 >=x
         if x < 2:
             return x
-        i = 2 #反向找结果
-        while i * i <= x:
-            i += 1
-        
-        return i -1
+        left, right = 0, x // 2
+        while left <= right:
+            mid = left + (right - left) // 2
+            square = mid * mid
+            if square == x:     #找到开根整数
+                return mid
+            elif square < x:    #更新下界
+                left = mid + 1
+            else:
+                right = mid - 1 #更新上界
+
+        return right    #stop condition: left > right. The result is in [right,left], so right is the integer of the down side.
