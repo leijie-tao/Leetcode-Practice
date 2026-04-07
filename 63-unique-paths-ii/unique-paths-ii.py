0435a51d0946900d7948ba11566695b129910a8a
LeijieTao <taoleijie623@gmail.com>
2026-04-07 11:07:52 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (93.77%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -3,13 +3,13 @@ class Solution:
         if not obstacleGrid or obstacleGrid[0][0] == 1:
             return 0
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0] * n for _ in range(m)]
-        dp[0][0] = 1
+        dp = [[0] * n for _ in range(m)]  #dp[i][j] means all unique paths to reach (i, j)
+        dp[0][0] = 1    #Base case
 
+        #Initialization: Loop through the grid and check the first raw and column, and record the number of ways to reach the coordinate. They are different from normal cooridinates since there is only one direction to reach them.
         for i in range(1, m):
             if obstacleGrid[i][0] == 0 and dp[i-1][0] == 1:
                 dp[i][0] = 1
-                
         for j in range(1, n):
             if obstacleGrid[0][j] == 0 and dp[0][j-1] == 1:
                 dp[0][j] = 1
