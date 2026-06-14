76de76d259d36e6654e1c31e12ba3ee62a9c5489
LeijieTao <tao.lei@northeastern.edu>
2026-06-14 16:35:35 +0800

    Time: 15 ms (40.35%) | Memory: 31.3 MB (72.12%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+        m = len(set(nums))
+        if m == n:
+            return False
+        else:
+            return True
