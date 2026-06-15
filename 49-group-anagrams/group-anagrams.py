017911ae70df372155b8b492306629e1f9ed848d
LeijieTao <tao.lei@northeastern.edu>
2026-06-15 17:34:45 +0800

    Time: 15 ms (43.87%) | Memory: 23.9 MB (17.87%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,26 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # if len(strs) <= 1:
+        #     return [strs]
+        
+        # record = defaultdict(list)      #Create a map with default value []
+        # for s in strs:
+        #     key = "".join(sorted(s))    # Take the same string as the key.    Notice: sorted() needs klogk
+        #     record[key].append(s)       # Record the value with the same string
+        
+        # return list(record.values())
+
+
+
+
         if len(strs) <= 1:
             return [strs]
         
-        record = defaultdict(list)      #Create a map with default value []
+        record = defaultdict(list)     
         for s in strs:
-            key = "".join(sorted(s))    # Take the same string as the key
-            record[key].append(s)       # Record the value with the same string
+            count = [0] * 26        # Refine the way of key. Loop through each element and record the count list.
+            for c in s:
+                count[ord(c) - ord("a")] += 1  
+            record[tuple(count)].append(s)      # Transfer list into tuple to be used as a key
         
         return list(record.values())
