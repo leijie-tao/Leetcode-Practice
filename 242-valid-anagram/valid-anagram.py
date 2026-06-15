cd3ec488f8cee832bb38c0178f0991ba8fb8b0de
LeijieTao <tao.lei@northeastern.edu>
2026-06-15 16:30:45 +0800

    Time: 7 ms (91.10%) | Memory: 19.5 MB (44.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,20 +1,28 @@
+from collections import Counter
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        map = {}       #Record character and its frequency
-        for char in s:
-            if char not in map:
-                map[char] = 1
-            else:
-                map[char] += 1
-        for char in t:
-            if char not in map:
-                return False
-            map[char] -= 1
+        return Counter(s) == Counter(t)     
+        #Counter() will loop through the string, count for each element, and return{"a": 3, "g": 1,...}
 
-        for m in map:
-            if map[m] != 0:
-                return False
-        return True
+
+
+        ## Hash map
+        # if len(s) != len(t):
+        #     return False
+        
+        # map = {}       #Record character and its frequency
+        # for char in s:
+        #     if char not in map:
+        #         map[char] = 1
+        #     else:
+        #         map[char] += 1
+        # for char in t:
+        #     if char not in map:
+        #         return False
+        #     map[char] -= 1
+
+        # for m in map:
+        #     if map[m] != 0:
+        #         return False
+        # return True
