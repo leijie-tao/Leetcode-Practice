beea68c8c5a116d4662c8124931e242beb18440a
LeijieTao <tao.lei@northeastern.edu>
2026-06-20 17:47:13 +0800

    Time: 47 ms (91.91%) | Memory: 29.7 MB (14.27%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        l, r = 0, len(height)-1  #两段向中间的双指针
-        max_amount = 0
-        while l < r:
-            curr_amount = min(height[l], height[r]) * (r - l)   #容量 = 短板 * 指针距离
-            max_amount = max(max_amount, curr_amount)
-            if height[r] < height[l]:   #移动短板
-                r -= 1
+        left, right = 0, len(height) - 1
+        maxWater = 0                                        #Maintain a max value
+        while left < right:
+            current = min(height[left], height[right]) * (right - left)     #The area = shorter height * width
+            if current > maxWater:                          #Update maxWater. Or use max(maxWater, curernt)
+                maxWater = current
+            if height[left] >= height[right]:               #Move the shorter height
+                right -= 1
             else:
-                l += 1
-        return max_amount
+                left += 1
+        return maxWater
+            
