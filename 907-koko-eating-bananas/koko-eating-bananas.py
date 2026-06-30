e101921eca1dfa76c37bbb34da85f0a5d7c0f1c6
LeijieTao <tao.lei@northeastern.edu>
2026-06-30 16:28:17 +0800

    Time: 163 ms (77.78%) | Memory: 20.5 MB (79.69%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,17 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        #速度为k：每次最少吃1个，最多吃max(piles)个 ——> 求刚好满足要求的目标速度
-        left, right = 1, max(piles)
-        while left <= right:
-            mid = left + (right - left)//2
-            total_hours = 0
-            #遍历累加计算每堆需要的时间，math.ceil()向上取整
-            for p in piles:
-                total_hours += math.ceil(p / mid)
-            #total_hours小于则排除较快的速度，大于排除较慢的速度
-            if total_hours <= h:
-                right = mid -1
-            else:
-                left = mid + 1
+        # Binary Search: Find the target speed between min_speed and max_speed
+        min_speed = 1
+        max_speed = max(piles)
 
-        return left
+        while min_speed <= max_speed:
+            mid = min_speed + (max_speed - min_speed) // 2
+            #Calculate the hours needed for finishing all the piles
+            hours_need = 0
+            for pile in piles:
+                hours_need += math.ceil(pile / mid)         #math.ceil() is round up to an integer
+
+            if hours_need <= h:     #Notice: Don't stop when ==. Need to find the minimum integer.
+                max_speed = mid - 1
+            else:
+                min_speed = mid + 1
+
+        return min_speed
