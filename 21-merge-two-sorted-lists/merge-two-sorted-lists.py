0aa7ddf5e9991257616cf9d85bbb66dce3126413
LeijieTao <tao.lei@northeastern.edu>
2026-07-01 16:28:27 +0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (71.99%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,18 +5,26 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy =  ListNode(-1) #创建虚拟头节点，可直接以dummy.next为头节点，省去判断l1/l2谁是头节点
-        curr = dummy
-        while list1 and list2:
-            if list1.val <= list2.val: 
-                curr.next = list1 #谁更小，把谁接在curr后
-                list1 = list1.next #同时移动到list1的下一个节点
+        dummy = ListNode(0)             # Use dummy node to create a new linked list
+        tail = dummy                    # Similar with nxt pointer
+    
+        while list1 and list2:          # If two lists exist, compare the value and add the list to the end of new list.
+            if list1.val <= list2.val:
+                tail.next = list1
+                tail = tail.next        # Move the tail and next node of list
+                list1 = list1.next 
             else:
-                curr.next = list2
-                list2 = list2.next
-            curr = curr.next   #移动curr到下一个节点
-        curr.next = list1 if list1 else list2  #其中一个list走完后，直接把剩下的接在curr后
-        return dummy.next
-                
+                tail.next = list2
+                tail = tail.next
+                list2 = list2.next 
+        
+        # If there is still a list, add it to the end.
+        if list1:
+            tail.next = list1
+        else:
+            tail.next = list2
+        
+        return dummy.next           #dummy.next is the head
 
 
+    
