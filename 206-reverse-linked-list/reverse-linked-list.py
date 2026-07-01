cc5bbd1fdb9d28a6ce0eea626d2d1f57de600af3
LeijieTao <tao.lei@northeastern.edu>
2026-07-01 15:57:29 +0800

    Time: 0 ms (100.00%) | Memory: 20.5 MB (64.97%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -5,11 +5,12 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pre = None #初始化pre指针，定义cur指针指向头节点
-        cur = head 
-        while cur:
-            temp = cur.next #暂存下节点
-            cur.next = pre #反转，指针指向前
-            pre = cur  #更新pre和cur
-            cur = temp
-        return pre
+        # Key point: don't lose the rest part when reverse the linked list 
+        pre = None          #After reversing, head will become the last node and point to None, which is pre.
+        cur = head          #Start from head
+        while cur:              
+            nxt = cur.next  #Save the next node first.
+            cur.next = pre  #Reverse
+            pre = cur       #Update pre and cur
+            cur = nxt
+        return pre          #After reversing, pre will be the head
