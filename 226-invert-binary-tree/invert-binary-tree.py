0036debe5b830d7b8ab928fa10d83943950e866b
LeijieTao <tao.lei@northeastern.edu>
2026-07-06 17:41:31 +0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.67%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -4,18 +4,40 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
+# # DFS: Go through each branch (Recursion).
+# class Solution:
+#     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+#         # Base case: return None when there is no more node
+#         if not root:
+#             return None
+        
+#         # At each node, swap the left and right children.
+#         root.left, root.right = root.right, root.left
+
+#         #Then recursively invert the left subtree（first) and right subtree(second).
+#         self.invertTree(root.left)
+#         self.invertTree(root.right)
+
+#         return root
+
+
+# BFS: Go through each layer (Queue).
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        # Base case: return None when there is no more node
         if not root:
             return None
+        #Create a queu, and add the [root]
+        queue = deque([root])
+        #while loop to deal with each layer
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+            # Add child nodes into the queue
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
         
-        # At each node, swap the left and right children.
-        root.left, root.right = root.right, root.left
-
-        #Then recursively invert the left subtree and right subtree.
-        self.invertTree(root.left)
-        self.invertTree(root.right)
-
         return root
-        
