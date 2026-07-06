402c53deeb462fdd9dcb0a41264ad865575f911b
LeijieTao <tao.lei@northeastern.edu>
2026-07-06 16:38:32 +0800

    Time: 138 ms (35.13%) | Memory: 78.9 MB (7.16%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,45 +1,60 @@
-class Node:  #创建doubly-linked list双向指针pre/next，通过prev指针在O(1)时间删除指定节点
+# Use hashmap to store the key and the node. ——> Find the target node by key.
+# Linked list maintain the order. ——> Find the least recently used node.
+class Node:  
     def __init__(self, key = None, val = None):
         self.key = key
         self.val = val
-        self.prev = None
+        self.prev = None        #Create doubly-linked list with pre/next pointers.
         self.next = None
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
         self.map = {}
-        # 虚拟头尾节点，用于定位最新（头）和最旧（尾），同时保证真实节点都在中间
-        self.head = Node()
-        self.tail = Node()
-        self.head.next = self.tail
+        # Create an empty linked list with virtual head/tail nodes to track the real head(head.next) and real tail(tail.prev)
+        self.head = Node()           #virtual node
+        self.tail = Node()           #virtual node
+        self.head.next = self.tail   
         self.tail.prev = self.head
 
-    def get(self, key: int) -> int: #每次获取节点，则把它从原位置拿出，并最新到虚拟头节点后
+
+
+
+    # Remove a node: Link the previous one with the next one.
+    def remove(self, node): 
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    # Insert : Add a node in front of whole list and behind the virtual head nodes
+    def insert(self, node): 
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+
+
+
+    # Get: take the node out, add it to the front, and return the node.val
+    def get(self, key: int) -> int: 
         if key not in self.map:
             return -1
         self.remove(self.map[key])
         self.insert(self.map[key])
         return self.map[key].val
 
-    def remove(self, node): #删除节点，跨过当前节点连接前后节点
-        node.prev.next = node.next
-        node.next.prev = node.prev
-
-    def insert(self, node): #把节点插入虚拟头节点后面（最新位置）
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
-
+    
     def put(self, key: int, value: int) -> None:
-        if key in self.map:               #key已存在，则更新值，同时把节点移动到虚拟头节点后
+        # If the node exists, update the value and position.
+        if key in self.map:               
             self.map[key].val = value 
             self.remove(self.map[key])
             self.insert(self.map[key])
-        else:                           #不存在则添加新的节点放入最新位置，若超容量则移动尾部节点并删除其key
+        # Otherwise, record the node and add it to the front.
+        else:                           
             self.map[key] = Node(key, value)
             self.insert(self.map[key])
+            # Also check the capacity. Remove the tail.prev when overload.
             if len(self.map) > self.capacity:
                 last_node = self.tail.prev
                 self.remove(last_node)
