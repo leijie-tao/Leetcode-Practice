a43a2f804ae70e58cfdee09438715abbbb567b87
LeijieTao <tao.lei@northeastern.edu>
2026-07-12 23:21:42 +0800

    Time: 1 ms (85.10%) | Memory: 22.4 MB (18.13%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -5,23 +5,43 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        # Stop condition
-        if not root:
-            return 0
-        #How to return the diameter of current layer? 
-        #1.Get the max height of each subtree to calculate the diameter
-        left_height = self.maxHeight(root.left)
-        right_height = self.maxHeight(root.right)
-        diameter = left_height + right_height
-        #2.Return the max value of current diameter, left-subtree diameter, and right-subtree diameter 
-        left_diameter = self.diameterOfBinaryTree(root.left)
-        right_diameter = self.diameterOfBinaryTree(root.right)
-        return max(diameter, left_diameter, right_diameter)
+    # #Brute force:
+    # def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+    #     # Stop condition
+    #     if not root:
+    #         return 0
+    #     #How to return the diameter of current layer? 
+    #     #1.Get the max height of each subtree to calculate the diameter
+    #     left_height = self.maxHeight(root.left)
+    #     right_height = self.maxHeight(root.right)
+    #     diameter = left_height + right_height
+    #     #2.Return the max value of current diameter, left-subtree diameter, and right-subtree diameter 
+    #     left_diameter = self.diameterOfBinaryTree(root.left)
+    #     right_diameter = self.diameterOfBinaryTree(root.right)
+    #     return max(diameter, left_diameter, right_diameter)
     
-    def maxHeight(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        l = self.maxHeight(root.left)
-        r = self.maxHeight(root.right)
-        return max(l, r) + 1
+    # def maxHeight(self, root: Optional[TreeNode]) -> int:
+    #     if not root:
+    #         return 0
+    #     l = self.maxHeight(root.left)
+    #     r = self.maxHeight(root.right)
+    #     return max(l, r) + 1
+
+
+
+    #DFS: Use a globle variable to avoid recomputing heights
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        #Use dfs to get the max depth and update max diameter during the process
+        def dfs(root):
+            nonlocal res
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            # Update the max diameter
+            res = max(res, left + right)
+            return 1 + max(left, right)
+
+        dfs(root)
+        return res
