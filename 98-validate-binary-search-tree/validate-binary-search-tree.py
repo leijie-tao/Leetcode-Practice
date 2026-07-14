c557cdbcda60ec820e78ac519938a526fa0727b6
LeijieTao <tao.lei@northeastern.edu>
2026-07-14 16:45:16 +0800

    Time: 0 ms (100.00%) | Memory: 20.9 MB (44.89%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -5,15 +5,35 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node, low, high):
-            if not node:
-                return True
-                
-            if not (low < node.val < high):
-                return False
-            left = dfs(node.left, low, node.val)
-            right = dfs(node.right, node.val, high)
-            return left and right
+    # # ------------- DFS: Check the relationship among node.val, low and high -----------------
+    # def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    #     def dfs(node, low, high):
+    #         if not node:
+    #             return True
+    #         # Each layer check the range
+    #         if not (low < node.val < high):
+    #             return False
+    #         # check the subtree with the updated boundary value
+    #         return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+        
+    #     return dfs(root, float('-inf'), float('inf'))
+    
 
-        return dfs(root, float('-inf'), float('inf'))
+    # ---------- BFS: Add each node into queue, regarding its parent as boundary -----------------
+    # node.left—> (-∞, node.val)    node.val—>(-∞, +∞)      node.right—>(node.val, +∞)
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        # Add root and its boundary into queue
+        queue = deque([(root, float("-inf"), float("inf"))])
+        while queue:
+            # Pop and check the order
+            node, left, right = queue.popleft()
+            if not (left < node.val < right):
+                return False
+            # Add the sub-nodes and their boundary into queue
+            if node.left:
+                queue.append((node.left, left, node.val))     #(-∞, node.val)
+            if node.right:
+                queue.append((node.right, node.val, right))   #(node.val, +∞)
+        return True
