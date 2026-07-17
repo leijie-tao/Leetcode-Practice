97d2d480c48d061f5524797617019c2709df3822
LeijieTao <tao.lei@northeastern.edu>
2026-07-17 16:49:48 +0800

    Time: 63 ms (69.75%) | Memory: 21.9 MB (82.87%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -6,12 +6,37 @@
 #         self.right = None
 
 class Solution:
+    ## ------------ DFS : check a node has valid return from both sub-trees ----------------------
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+#         # Base case: return p/q or None
+#         if not root or root == p or root == q:
+#             return root
+
+#         # Each layer: check if both left-tree and right-tree return value
+#         left = self.lowestCommonAncestor(root.left, p, q)
+#         right = self.lowestCommonAncestor(root.right, p, q)
+#         if right and left:
+#             return root
+
+#         # Either left or right has value, return it to upper layer
+#         return left or right
+
+
+
+
+ #------------------- Iteration: use the nature of BST ----------------------
+ # Common ancestor ——> q <= root.val <= p
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        #Lowest Common Ancestor is the node that the value is between p.val and q.val
-        while root:
-            if p.val < root.val and q.val < root.val:
-                root = root.left
-            elif p.val > root.val and q.val > root.val:
-                root = root.right
+        cur = root
+
+        while cur:
+            # If both p and q are larger than cur, move cur to the right sub-tree
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            # If both p and q are smaller than cur, move cur to the left sub-tree
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+            # Otherwise, curr is between p and q
             else:
-                return root
+                return cur
+    
