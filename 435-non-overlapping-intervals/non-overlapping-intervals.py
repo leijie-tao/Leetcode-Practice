291ea6e3dc936d493f8b6bd458909fb73e594809
LeijieTao <tao.lei@northeastern.edu>
2026-08-29 02:21:41 +0800

    Time: 68 ms (77.63%) | Memory: 49 MB (79.14%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -1,13 +1,27 @@
+# class Solution:
+#     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+#         intervals.sort(key = lambda x: x[0])
+#         res = [intervals[0]]
+#         count = 0
+#         for cur in intervals[1:]:
+#             if cur[0] < res[-1][1]:
+#                 count += 1
+#                 # update res[-1], and keep smaller end to get minimum removal
+#                 res[-1] = min(res[-1], cur, key=lambda x: x[1])
+#             else:
+#                 res.append(cur)
+#         return count
+
+
+# ------------------ Sort by end first, and delete big end when meet overlap ----------------
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key = lambda x: x[0])
+        intervals.sort(key = lambda x: x[1])
         res = [intervals[0]]
         count = 0
         for cur in intervals[1:]:
             if cur[0] < res[-1][1]:
                 count += 1
-                # update res[-1], and keep smaller end to get minimum removal
-                res[-1] = min(res[-1], cur, key=lambda x: x[1])
             else:
                 res.append(cur)
         return count
