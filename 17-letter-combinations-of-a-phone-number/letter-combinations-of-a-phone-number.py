6acc223bf08b5634565aa67905a7919da7eca53f
LeijieTao <tao.lei@northeastern.edu>
2026-09-11 07:27:54 +0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (9.85%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -2,18 +2,26 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
-        phone = {"2": "abc", "3":"def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"} #回溯与哈希表结合
+
+        # Create the map to look up
+        phone = {"2": "abc", "3":"def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"} 
         res = []
-        def backtrack(index, path):
+        path = []
+
+        # Use index to locate the digit and look up for its characters
+        def backtrack(index):
+            # When path is full, add it to res
             if index == len(digits):
-                res.append("".join(path)) #把path(list)里的元素连接起来，整体作为元素放入res
+                res.append("".join(path))
                 return
-            letters = phone[digits[index]] #根据哈希表定位字母串
-            for char in letters: #遍历index位的每个字母
+            
+            # Get the list of all characters of current digits
+            letters = phone[digits[index]]
+            for char in letters: 
                 path.append(char)
-                backtrack(index+1, path) #index位的字母与index+1位的字母组合
+                backtrack(index+1)  # Move to the next digit
                 path.pop()
 
-        backtrack(0, [])
+        backtrack(0)
         return res
 
