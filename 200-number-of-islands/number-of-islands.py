9eb2ccbe60f2f7226d903aa4b819cf1884d82483
LeijieTao <tao.lei@northeastern.edu>
2026-09-14 02:49:30 +0800

    Time: 295 ms (6.41%) | Memory: 22.2 MB (31.50%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,29 +1,47 @@
-#BFS解法
+#DFS解法
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        rows, columns = len(grid), len(grid[0])
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        visited = [[False for _ in range(columns)] for _ in range(rows)]
-        count = 0
+        rows, cols = len(grid), len(grid[0])
+        used = [[False] * cols for _ in range(rows)]
+        res = 0
 
-        #将第一个点的周边所有陆地都记录至访问表
+        # DFS visit all cells around (r,c), and mark them as used
         def dfs(r, c):
-            visited[r][c] = True
-            for dr, dc in directions:
-                nr = r + dr
-                nc = c + dc
-                if (0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == "1" and visited[nr][nc] == False):
-                    dfs(nr, nc) #节点在边界内、是陆地、还未标记，则对它展开搜查
-        #遍历每行每列，寻找符合要求的陆地首点
-        for r in range(rows):
-            for c in range(columns):
-                if grid[r][c] == "1" and visited[r][c] == False:
-                    count += 1
-                    dfs(r, c) #标记同一陆地的所有点
-        
-        return count
+            # Stop condition: 1-2.out of range  3.visited  4.meet water
+            if r not in range(rows) or c not in range(cols) or used[r][c] == True or grid[r][c] == "0":
+                return
+            used[r][c] = True
+            direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for dr, dc in direction:
+                dfs(r + dr, c + dc)
 
+
+        for r in range(rows):
+            for c in range(cols):
+                # If it's a new island, start DFS
+                if grid[r][c] == "1" and used[r][c] == False:
+                    dfs(r, c)
+                    res += 1
+        return res
+
+
+
+        # def bfs(r, c):
+        #     q = deque([(r, c)])      # [tuple]
+        #     used[r][c] = True
+        #     while q:
+        #         cr, cc = q.popleft()
+        #         direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        #         for dr, dc in direction:
+        #             nr, nc = cr + dr, cc + dc
+                      ## Add valid cells into queue to search for their neighbors
+        #             if (0 <= nr < rows and
+        #              0 <= nc < col and
+        #               used[nr][nc] == False and
+        #                grid[nr][nc] == "1"):
+        #                 used[nr][nc] = True
+        #                 q.append((nr, nc))
 
 
