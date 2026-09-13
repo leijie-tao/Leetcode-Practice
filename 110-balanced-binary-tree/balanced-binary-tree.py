800300b8df98b0441e9323168f5514d46f039550
LeijieTao <tao.lei@northeastern.edu>
2026-09-14 06:09:39 +0800

    Time: 1 ms (60.58%) | Memory: 20.4 MB (38.99%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -28,25 +28,41 @@ class Solution:
 
 
 
-    #-----Combine height calculation with balance judgement.  O(n^2) ——> O(n)----------
-    # To combine checking process and height calculation, use -1 to mark imbalanced.
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        # Use check to return -1 for imbalanced subtree and reurn the height for balanced subtree
-        def check(node):
-            if not node:
-                return 0
-            # If the subtree is imbalanced, return -1 directly
-            left = check(node.left)
-            if left == -1:
-                return -1
-            right = check(node.right)
-            if right == -1:
-                return -1
-            # If the subtree is balanced, check the difference of current layer
-            if abs(left - right) > 1:
-                return -1
-            # If there is no imbalanced tree, return the current height
-            else:
-                return max(left, right) + 1
+    # #-----Combine height calculation with balance judgement.  O(n^2) ——> O(n)----------
+    # # To combine checking process and height calculation, use -1 to mark imbalanced.
+    # def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    #     # Use check to return -1 for imbalanced subtree and reurn the height for balanced subtree
+    #     def check(node):
+    #         if not node:
+    #             return 0
+    #         # If the subtree is imbalanced, return -1 directly
+    #         left = check(node.left)
+    #         if left == -1:
+    #             return -1
+    #         right = check(node.right)
+    #         if right == -1:
+    #             return -1
+    #         # If the subtree is balanced, check the difference of current layer
+    #         if abs(left - right) > 1:
+    #             return -1
+    #         # If there is no imbalanced tree, return the current height
+    #         else:
+    #             return max(left, right) + 1
         
-        return check(root) != -1
+    #     return check(root) != -1
+
+
+    # --------------- DFS ----------------
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return [True, 0]
+            # Get the result from subtrees first, and then decide what to reurn
+            left, right = dfs(root.left), dfs(root.right)
+            if abs(left[1] - right[1]) > 1 or left[0] == False or right[0] == False:
+                return [False, -1]
+            else:
+                height = max(left[1], right[1]) + 1
+                return [True, height]
+
+        return dfs(root)[0]
