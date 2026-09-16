ee8e6a33d1fac618abcdf87662d6eb9bfc8b2916
LeijieTao <tao.lei@northeastern.edu>
2026-09-16 13:16:51 +0800

    Time: 3 ms (75.63%) | Memory: 19.3 MB (42.76%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,31 +1,34 @@
 class Solution:
+    # 多个点同时bfs扩散  ----> 先数出多少个烂橘子
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows, columns = len(grid), len(grid[0])
-        queue = deque()
-        fresh_count = 0
-        #初始化，收集所有烂橘子的位置，记录新鲜橘子的数量
-        for r in range(rows):
-            for c in range(columns):
-                if grid[r][c] == 2:
-                    queue.append((r,c))
-                elif grid[r][c] == 1:
-                    fresh_count += 1
-        if fresh_count == 0:
-            return 0
-        
-        minutes = -1
-        while queue: #所有烂橘子同时开始扩散
-            minutes += 1 
-            for _ in range(len(queue)): #弹出当前每个烂橘子，并查找他们的四周
-                r, c = queue.popleft()
-                for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                    nr, nc = r + dr, c + dc
-                    # 如果四周在边界内，且有新鲜橘子，则变腐烂，并加入队列
-                    if 0 <= nr < rows and 0 <= nc < columns and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2
-                        fresh_count -= 1
-                        queue.append((nr, nc))
-                        
-        return minutes if fresh_count == 0 else -1
+        rows, cols = len(grid), len(grid[0])
+        minutes = 0
+        q = deque()     # rotten oranges -> queue
+        fresh = 0       # fresh oranges -> count to see when to stop
 
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
+
+        while q and fresh > 0:     # fresh > 0 防止最后一批入队(全部腐烂)后多算 minutes += 1
+            n = len(q)
+            for _ in range(n):
+                r, c = q.popleft()
+                directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                for dr, dc in directions:
+                    new_r = r + dr
+                    new_c = c + dc
+                    if new_r in range(rows) and new_c in range(cols) and grid[new_r][new_c] == 1:
+                        grid[new_r][new_c] = 2
+                        fresh -= 1
+                        q.append((new_r, new_c))
+            minutes += 1
+        
+        if fresh == 0:
+            return minutes
+        else:
+            return -1
 
