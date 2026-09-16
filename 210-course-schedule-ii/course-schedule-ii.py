584e4628dc49981874cdb265725cdde46a3046c9
LeijieTao <tao.lei@northeastern.edu>
2026-09-17 01:59:31 +0800

    Time: 3 ms (75.00%) | Memory: 20.5 MB (56.76%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,31 +1,30 @@
+# 1. Need to know if there is a prerequisite -----> indegree
+# 2. Need to know what's the next course  ------> adj
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        #创建入度表（记录cur的先修数量）、邻接表（记录pre后续课程）
-        indegree = [0] * numCourses
-        adjacency = [[] for _ in range(numCourses)]
-        for  cur, pre in prerequisites:
-            indegree[cur] += 1
-            adjacency[pre].append(cur)
+        res = []
+        indegree = [0] * numCourses              # record how many prerequisites are needed
+        adj = [[] for _ in range(numCourses)]    # find next eligible course
+        for crs, pre in prerequisites:
+            adj[pre].append(crs)
+            indegree[crs] += 1
 
-        #构建队列，可修课程依次入队
-        lst = []
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                lst.append(i)
-        queue = deque(lst)
+        # Iterate all courses, add eligible courses into queue
+        q = deque()
+        for n in range(numCourses):
+            if indegree[n] == 0:
+                q.append(n)
 
-        #bfs拓扑排序，弹出已修课程添加至result。遍历邻接表中当前pre的后续课程列表，更新入度，发现0入度再次入队
-        result = []
-        while queue:
-            pre = queue.popleft()
-            result.append(pre)
-            for cur in adjacency[pre]:
-                indegree[cur] -= 1
-                if indegree[cur] == 0:
-                    queue.append(cur)
+        # Deal with each course in order. (check if next course is eligible, and add it into queue)
+        while q:
+            c = q.popleft()
+            res.append(c)
+            for neighbor in adj[c]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
         
-        #检查是否存在环（课程修完）
-        if len(result) == numCourses:
-            return result
+        if len(res) == numCourses:
+            return res
         else:
             return []
