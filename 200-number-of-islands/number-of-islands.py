f56ddbc6cbbe6fc4f8148d45fa04fdaaa0a1863a
LeijieTao <tao.lei@northeastern.edu>
2026-09-17 12:22:59 +0800

    Time: 281 ms (10.70%) | Memory: 21.6 MB (73.31%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,47 +1,62 @@
-#DFS解法
+# #----------------- DFS : m * n -------------------
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+#         if not grid:
+#             return 0
+#         rows, cols = len(grid), len(grid[0])
+#         visit = [[False] * cols for _ in range(rows)]
+#         count = 0
+#         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+#         def dfs(r, c):
+#             if r not in range(rows) or c not in range(cols) or visit[r][c] == True or grid[r][c] == "0":
+#                 return
+#             visit[r][c] = True
+#             for dr, dc in directions:
+#                 dfs(r + dr, c + dc)
+
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if grid[r][c] == "1" and visit[r][c] == False:
+#                     dfs(r, c)
+#                     count += 1
+
+#         return count
+
+
+
+
+
+
+# ------------------ BFS: m * n  --------------------
         if not grid:
             return 0
         rows, cols = len(grid), len(grid[0])
-        used = [[False] * cols for _ in range(rows)]
-        res = 0
+        visit = [[False] * cols for _ in range(rows)]
+        count = 0
 
-        # DFS visit all cells around (r,c), and mark them as used
-        def dfs(r, c):
-            # Stop condition: 1-2.out of range  3.visited  4.meet water
-            if r not in range(rows) or c not in range(cols) or used[r][c] == True or grid[r][c] == "0":
-                return
-            used[r][c] = True
-            direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-            for dr, dc in direction:
-                dfs(r + dr, c + dc)
-
+        def bfs(r, c):
+            q = deque()
+            q.append((r,c))
+            visit[r][c] = True
+            while q:
+                r, c = q.popleft()
+                directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                for dr, dc in directions:
+                    new_r = dr + r
+                    new_c = dc + c
+                    if new_r in range(rows) and new_c in range(cols) and grid[new_r][new_c] == "1" and visit[new_r][new_c] == False:
+                        q.append((new_r, new_c))
+                        visit[new_r][new_c] = True
 
         for r in range(rows):
             for c in range(cols):
-                # If it's a new island, start DFS
-                if grid[r][c] == "1" and used[r][c] == False:
-                    dfs(r, c)
-                    res += 1
-        return res
+                if grid[r][c] == "1" and visit[r][c] == False:
+                    bfs(r, c)
+                    count += 1
 
+        return count
+        
 
-
-        # def bfs(r, c):
-        #     q = deque([(r, c)])      # [tuple]
-        #     used[r][c] = True
-        #     while q:
-        #         cr, cc = q.popleft()
-        #         direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        #         for dr, dc in direction:
-        #             nr, nc = cr + dr, cc + dc
-                      ## Add valid cells into queue to search for their neighbors
-        #             if (0 <= nr < rows and
-        #              0 <= nc < col and
-        #               used[nr][nc] == False and
-        #                grid[nr][nc] == "1"):
-        #                 used[nr][nc] = True
-        #                 q.append((nr, nc))
 
 
